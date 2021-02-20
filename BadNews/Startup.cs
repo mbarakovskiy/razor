19e5ca346cc1f9bf87a6042cfd317c6868b277ed
Домашних Иван <digi@skbkontur.ru>
2021-02-20 19:16:52 +0500

    Solved 3.2.

--- BadNews/Startup.cs
+++ BadNews/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using BadNews.ModelBuilders.News;
 using BadNews.Repositories.News;
+using BadNews.Validation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,7 @@ namespace BadNews
         {
             services.AddSingleton<INewsRepository, NewsRepository>();
             services.AddSingleton<INewsModelBuilder, NewsModelBuilder>();
+            services.AddSingleton<IValidationAttributeAdapterProvider, StopWordsAttributeAdapterProvider>();
             var mvcBuilder = services.AddControllersWithViews();
             if (env.IsDevelopment())
                 mvcBuilder.AddRazorRuntimeCompilation();
