2bc23170280b2ee4486b501aa47411d978a2f5ad
Mikhail Barakovskiy <mbarakovskiy@gmail.com>
2021-10-19 18:26:17 +0500

    Done Task 2.1

--- BadNews/Startup.cs
+++ BadNews/Startup.cs
@@ -35,7 +35,9 @@ namespace BadNews
         {
             services.AddSingleton<INewsRepository, NewsRepository>();
             services.AddSingleton<INewsModelBuilder, NewsModelBuilder>();
-            services.AddControllersWithViews();
+            var mvcBuilder = services.AddControllersWithViews();
+            if (env.IsDevelopment())
+                mvcBuilder.AddRazorRuntimeCompilation();
         }
 
         // В этом методе конфигурируется последовательность обработки HTTP-запроса
@@ -50,15 +52,10 @@ namespace BadNews
             app.UseSerilogRequestLogging();
             app.UseStatusCodePagesWithReExecute("/StatusCode/{0}");
 
-            app.Map("/news", newsApp =>
+            app.Map("/news/fullarticle", fullArticleApp =>
             {
-                newsApp.Map("/fullarticle", fullArticleApp => { fullArticleApp.Run(RenderFullArticlePage); });
-
-                newsApp.Run(RenderIndexPage);
+                fullArticleApp.Run(RenderFullArticlePage);
             });
-
-            app.MapWhen(context => context.Request.Path == "/", rootPathApp => { rootPathApp.Run(RenderIndexPage); });
-
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
@@ -68,7 +65,7 @@ namespace BadNews
                     action = "StatusCode"
                 });
 
-                endpoints.MapControllerRoute("default", "{controller}/{action}");
+                endpoints.MapControllerRoute("default", "{controller=News}/{action=Index}");
             });
 
             // Остальные запросы — 404 Not Found
